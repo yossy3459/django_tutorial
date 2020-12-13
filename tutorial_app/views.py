6b579d14f035461e405dd4c17c6a843fd88cfae7
yossy3459 <yoshi3459@outlook.com>
2020-12-14 00:11:22 +0900

    query param

--- tutorial_app/views.py
+++ tutorial_app/views.py
@@ -3,4 +3,5 @@ from django.http import HttpResponse
 
 # Create your views here.
 def index(request):
-    return HttpResponse("Hello Django!")
+    msg = request.GET['msg']
+    return HttpResponse(f"Query Param.: {msg}")
